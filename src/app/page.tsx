e5796a48b5b5a43f37b32da3e29658da090f6d20
laposhko <laposhko0@gmail.com>
2024-10-20 18:24:34 +0300

    added note about credentials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 export default function Home() {
   return (
-    <main>
+    <main className="flex h-screen flex-col justify-center items-center gap-4">
+      <h1 className="text-3xl">Welcome to our application</h1>
       <Link
         href={'/login'}
-        className="block bg-gray-800 text-white w-60 h-10 rounded text-center"
+        className="flex justify-center items-center bg-gray-800 text-white w-60 h-10 rounded "
       >
         Log in
       </Link>
